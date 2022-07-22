4af5f6bd6768338659e05ae051027e740397331f
strahinja-plassido <strahinja.cvijanovic@plassido.com>
2022-07-22 23:30:00 +0200

    cors fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
@@ -21,6 +23,11 @@ async function bootstrap() {
   .addTag('Training')
   .addTag('WorkTime')
   .build();
+
+  const cors = require('cors')
+  app.use(cors({
+    origin: "http://localhost:3000",
+  }))
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
